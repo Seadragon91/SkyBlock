400741ba10f965629799e653f22d2b8d821a4ef3
Lukas Pioch <lukas@zgow.de>
2020-03-08 11:35:12 +0100

    Fixed loading of island.schematic

--- SkyBlock.lua
+++ SkyBlock.lua
@@ -41,16 +41,16 @@ function Initialize(Plugin)
 	LANGUAGE_DEFAULT = "english.ini"
 	LANGUAGE_OTHERS = 1
 
-	ISLAND_AREA = cBlockArea()
-	if not(ISLAND_AREA:LoadFromSchematicFile(PLUGIN:GetLocalFolder() .. "/" .. ISLAND_SCHEMATIC)) then
-		ISLAND_AREA = nil
-	end
-
 	-- Load all lua files
 	LoadLuaFiles()
 
 	-- Load Config file
 	LoadConfiguration()
+
+	ISLAND_AREA = cBlockArea()
+	if not(ISLAND_AREA:LoadFromSchematicFile(PLUGIN:GetLocalFolder() .. "/" .. ISLAND_SCHEMATIC)) then
+		ISLAND_AREA = nil
+	end
 
 	-- Check for world <WORLD NAME>
 	SKYBLOCK = cRoot:Get():GetWorld(WORLD_NAME)
